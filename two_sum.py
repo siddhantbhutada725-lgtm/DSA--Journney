05daa177fb37366098d7b2ddee4e21a423624be8
siddhantbhutada725-lgtm <siddhantbhutada725@gmail.com>
2026-03-26 21:41:29 +0530

    Optimised two sums on O(n) using hash maps

--- two_sum.py
+++ two_sum.py
@@ -1,9 +1,8 @@
-class Solution:
-    def twoSum(self, nums, target):
-        # Finger 1: Starts at the beginning
-        for i in range(len(nums)):
-            # Finger 2: Scans the lockers ahead of Finger 1
-            for j in range(i + 1, len(nums)):
-                # If they add up to the target, return their indices
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+class solutions
+    def twoSums(self,nums,target):
+        seen_numbers={}
+        for i,nums in enumerate(nums):
+            complement = target - nums
+            if complement in seen_numbers:
+                return [seen_numbers[compliment],i]
+            seen_numbers[nums]=i
